8a748f76a06fb03dc47b507e199f4ee314ebd2e4
egiit <giefilth@gmail.com>
2023-12-04 11:56:05 +0700

    "REVISI query"

--- models/reports/auditTracking.mod.js
+++ models/reports/auditTracking.mod.js
@@ -1,13 +1,15 @@
-export const XmainTraceOrder = `SELECT 
+export const mainTraceOrder = `SELECT 
 NA.Production_Month PRODUCTION_MONTH,
 NA.Mfg_Site MANUFACTURING_SITE,
 NA.Customer_Name CUSTOMER_NAME,
+NA.Customer_Divison CUSTOMER_DIVISON,
 NA.Order_ID ORDER_NO,
 NA.Order_Reference_PONo ORDER_REFERENCE_PO_NO, 
+NA.MOID,
 NA.PO_Status PO_STATUS,
 NA.Product_Item_ID PRODUCT_IT,
 NA.Product_Item_Code ORDER_STYLE_DESCRIPTION,
-NA.MOID,
+NA.Order_Reference_PONo,
 NA.Item_Color_Name ITEM_COLOR_NAME,
 NA.ORDER_QTY,
 NA.MO_QTY MO_QTY,
@@ -15,8 +17,12 @@ NB.BUNDLE_QTY SEWING_IN_QTY,
 IFNULL((NB.BUNDLE_QTY-NA.MO_QTY), 0) SEWNGN_IN_VAR,
 NC.BUNDLE_QTY SEWING_OUT_QTY,
 IFNULL((NC.BUNDLE_QTY-NA.MO_QTY), 0) SEWNGN_OUT_VAR,
-ND.BUNDLE_QTY PACKINGG_IN_QTY,
+ND.BUNDLE_QTY PACKING_IN_QTY,
 IFNULL((ND.BUNDLE_QTY-NA.MO_QTY), 0) PACKING_IN_VAR,
+NE.PACKING_OUT,
+IFNULL((ND.BUNDLE_QTY-NA.MO_QTY), 0) PACKING_OUT_VAR,
+NF.PACKING_SLIP_QTY,
+IFNULL((NF.PACKING_SLIP_QTY-NA.MO_QTY), 0) PACKING_SLIP_VAR,
 NA.Shipped_Qty SHIPPED_QTY, -- HARUS GANTI DENGAN finish prod detail
 IFNULL((NA.Shipped_Qty-NA.MO_QTY), 0) SHIPPED_VAR
 FROM (
@@ -24,6 +30,7 @@ FROM (
         nb.Production_Month,
         nb.Mfg_Site,
         nb.Customer_Name,
+        nb.Customer_Divison,
         nb.Order_ID,
         nb.Order_Reference_PONo, 
         nb.PO_Status,
@@ -39,6 +46,7 @@ FROM (
         a.Production_Month,
         a.Mfg_Site,
         a.Customer_Name,
+        a.Customer_Divison,
         a.Order_ID,
         a.Order_Reference_PONo, 
         a.PO_Status,
@@ -67,33 +75,56 @@ FROM (
     GROUP BY a.Order_ID, a.MOID
 ) NB ON NB.ORDER_ID = NA.ORDER_ID AND NB.MOID = NA.MOID
 LEFT JOIN (
-SELECT 
-    a.Order_ID ORDER_ID, a.MOID, SUM(a.Bundle_Qty) BUNDLE_QTY
-FROM 
-sewing_out a 
-WHERE a.MOID IN (
-        SELECT 
-        v.MOID
-        FROM OrderPOListing v 
-        WHERE v.Production_Month IN (:listMonth)
-) 
-GROUP BY a.Order_ID, a.MOID
+	SELECT 
+	    a.Order_ID ORDER_ID, a.MOID, SUM(a.Bundle_Qty) BUNDLE_QTY
+	FROM 
+	sewing_out a 
+	WHERE a.MOID IN (
+	        SELECT 
+	        v.MOID
+	        FROM OrderPOListing v 
+	        WHERE v.Production_Month IN (:listMonth)
+	) 
+	GROUP BY a.Order_ID, a.MOID
 ) NC ON NC.ORDER_ID = NA.ORDER_ID AND NC.MOID = NA.MOID
 LEFT JOIN (
-SELECT 
-    a.Order_ID ORDER_ID, a.MOID, SUM(a.Bundle_Qty) BUNDLE_QTY
-FROM 
-packing_in a 
-WHERE a.MOID IN (
-        SELECT 
-        v.MOID
-        FROM OrderPOListing v 
-        WHERE v.Production_Month IN (:listMonth)
-) 
-GROUP BY a.Order_ID, a.MOID
-) ND ON ND.ORDER_ID = NA.ORDER_ID AND ND.MOID = NA.MOID`;
+	SELECT 
+	    a.Order_ID ORDER_ID, a.MOID, SUM(a.Bundle_Qty) BUNDLE_QTY
+	FROM 
+	packing_in a 
+	WHERE a.MOID IN (
+	        SELECT 
+	        v.MOID
+	        FROM OrderPOListing v 
+	        WHERE v.Production_Month IN (:listMonth)
+	) 
+	GROUP BY a.Order_ID, a.MOID
+) ND ON ND.ORDER_ID = NA.ORDER_ID AND ND.MOID = NA.MOID
+LEFT JOIN (
+	SELECT 
+	a.OrderID ORDER_ID,  a.OrderRefPONo, a.MOID, SUM(a.GoodQty) PACKING_OUT
+	FROM 
+	finishingProdDetail a 
+	WHERE a.MOID IN (
+	        SELECT 
+	        v.MOID
+	        FROM OrderPOListing v 
+	        WHERE v.Production_Month IN (:listMonth)
+	) 
+	GROUP BY a.OrderID,  a.MOID
+) NE ON NE.ORDER_ID = NA.ORDER_ID AND NE.MOID = NA.MOID
+LEFT JOIN (
+	SELECT 
+	    a.ORDER_ID, a.ORDER_REF_PO_No, b.MOID, SUM(a.PACKING_SLIP_QTY) PACKING_SLIP_QTY
+	FROM 
+	CustomerPackingSlip a
+	LEFT JOIN OrderPOListing b ON a.ORDER_PO_ID	= b.Order_PO_ID
+	WHERE b.Production_Month IN (:listMonth)
+	GROUP BY a.ORDER_ID, a.ORDER_REF_PO_No,  b.MOID
+)NF ON NF.ORDER_ID = NA.ORDER_ID AND NF.MOID = NA.MOID
+ORDER BY NA.Production_Month, NA.Customer_Name`;
 
-export const mainTraceOrder = `SELECT 
+export const XmainTraceOrder = `SELECT 
 NA.Production_Month PRODUCTION_MONTH,
 NA.Mfg_Site MANUFACTURING_SITE,
 NA.Customer_Name CUSTOMER_NAME,
