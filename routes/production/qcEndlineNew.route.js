b5b3f406e2c918a236ee3d1cb5e5c025537bf1ea
egiit <giefilth@gmail.com>
2024-08-15 14:12:58 +0700

    qcendline fix pendding

--- routes/production/qcEndlineNew.route.js
+++ routes/production/qcEndlineNew.route.js
@@ -25,7 +25,7 @@ router.get(
 );
 router.get("/schedule-size/:schDate/:sitename/:linename", getEndlineSchSize);
 router.get("/schedule-size-qr/:schDate/:sitename/:linename", getQrListActive);
-// router.get("/qr-pendding/:schDate/:sitename/:linename", getQrListPenddingNew);
+router.get("/qr-pendding/:schDate/:sitename/:linename", getQrListPenddingNew);
 router.get("/qr-selected/:barcodeSerial", getQrSelected);
 router.get("/qr-selected-defect/:barcodeSerial", getQrDefectList);
 router.get("/count-undo/:barcodeSerial/:userQc", getUndoCount);
