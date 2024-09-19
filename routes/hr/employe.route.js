42faa177334c0a19151d54d88878b5c437ffb0a3
Aan Kristanto Nugroho <43904651+aankristanto@users.noreply.github.com>
2024-09-19 16:50:16 +0700

    adding master route

--- routes/hr/employe.route.js
+++ routes/hr/employe.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getDeptAll, getEmployeAktif, getSubDeptAll, postNewEmploye } from "../../controllers/hr/employe.js";
+import { getDeptAll, getEmployeAktif, getPositionAll, getSalaryType, getSection, getSubDeptAll, postNewEmploye } from "../../controllers/hr/employe.js";
 import { getEventList, getRefGuest } from "../../controllers/hr/eventHr.js";
 import { CheckPassKey, GeneratePassKey, getLamaranByDate, getMasterAlamat, getMasterKabkota, getMasterKecamatan, getMasterKelurahan, getMasterProv, postApproveLamaran, postLamaran } from "../../controllers/hr/recruitment.js";
 import { getJobPosting, getJobPostingByID, postJobActive, updateJobPosting } from "../../controllers/hr/jobposting.js";
@@ -7,14 +7,17 @@ import { getApprovedPelamar } from "../../controllers/hr/acceptance.js";
 
 const router = express.Router();
 
-// departemen management
-router.get("/master-dept", getDeptAll);
-router.get("/master-subdept", getSubDeptAll);
+// master hr
 router.get("/master-address", getMasterAlamat);
 router.get("/master-address-provinsi", getMasterProv);
 router.get("/master-address-kabkota", getMasterKabkota);
 router.get("/master-address-kecamatan", getMasterKecamatan);
 router.get("/master-address-kelurahan", getMasterKelurahan);
+router.get("/master-dept", getDeptAll);
+router.get("/master-subdept", getSubDeptAll);
+router.get("/master-position", getPositionAll);
+router.get("/master-saltype", getSalaryType)
+router.get("/master-section", getSection);
 
 // job posting
 router.post("/post-active-job", postJobActive);
