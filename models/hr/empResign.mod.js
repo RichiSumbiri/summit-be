30127543b7e1355205f22bc35616d0f0b99f672a
Aan Kristanto Nugroho <43904651+aankristanto@users.noreply.github.com>
2024-11-12 15:39:33 +0700

    add tanggal spk sebagai search date

--- models/hr/empResign.mod.js
+++ models/hr/empResign.mod.js
@@ -23,7 +23,7 @@ FROM
 LEFT JOIN sumbiri_employee se ON se.Nik = spk.Nik 
 LEFT JOIN master_department md ON md.IdDept = se.IDDepartemen 
 LEFT JOIN master_position mp ON mp.IDPosition = se.IDPosisi 
-WHERE se.TanggalKeluar BETWEEN :startDate AND :endDate
+WHERE DATE(spk.CreateDate) BETWEEN :startDate AND :endDate
 ORDER BY spk.CreateDate DESC
 `;
 
