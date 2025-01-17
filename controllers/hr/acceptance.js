bafdc6aae07f1ad0a2ed4acacd9e5bede2c3e2d6
Aan Kristanto Nugroho <43904651+aankristanto@users.noreply.github.com>
2025-01-17 13:49:55 +0700

    add raw true on acceptance post new emp

--- controllers/hr/acceptance.js
+++ controllers/hr/acceptance.js
@@ -34,7 +34,7 @@ export const getApprovedPelamar = async(req,res) => {
 export const postNewEmp = async(req,res) => {
     try {
         const dataNewEmp            = req.body.dataNewEmp;
-        const checkExistingEMP      = await modelSumbiriEmployee.findAll({ where: { NikKTP: dataNewEmp.NikKTP, Nik: dataNewEmp.Nik }});
+        const checkExistingEMP      = await modelSumbiriEmployee.findAll({ where: { NikKTP: dataNewEmp.NikKTP, Nik: dataNewEmp.Nik }, raw: true });
         let postEmp;
 
         if(checkExistingEMP.length !== 0){
