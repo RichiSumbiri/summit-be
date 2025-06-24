ccf7bed89fef4b24478b460533c122cb642e5831
egiit <giefilt@gmail.com>
2025-06-24 22:45:29 +0700

    pre final ieCycleTIme

--- controllers/production/ie/IeCycleTime.js
+++ controllers/production/ie/IeCycleTime.js
@@ -1,10 +1,11 @@
 
 import db from "../../../config/database.js";
 import { QueryTypes, Op } from "sequelize";
-import {  ieCtDetailCount, IeCtGroupCount, IeCtMpProcesses, IeCycleTimeHeader, IeCycleTimeMp, IeObDetailCT, IeObFeaturesCT, IeObHeaderCT, qryGetFeatCt, qryGetIctMpProcces, qryGetListEffCt, qryGetObDetailCt } from "../../../models/ie/IeCT.mod.js";
+import {  ieCtDetailCount, IeCtGroupCount, IeCtMpProcesses, IeCtMpProcessGroup, IeCycleTimeHeader, IeCycleTimeMp, IeObDetailCT, IeObFeaturesCT, IeObHeaderCT, qryGetDataBarChart, qryGetFeatCt, qryGetIctMpProcces, qryGetIeCtMppOne, qryGetListEffCt, qryGetObDetailCt, qryListCtSite } from "../../../models/ie/IeCT.mod.js";
 import { qryGetEmpForIeCt } from "../../../models/hr/employe.mod.js";
 import { dbSPL } from "../../../config/dbAudit.js";
 import { IeObDetail, IeObFeatures, IeObHeader } from "../../../models/ie/IeOb.mod.js";
+import { averageCol } from "../../util/Utility.js";
 
 //query get dailly eff report
 export const getSewRepEffforCt = async (req, res) => {
@@ -66,10 +67,129 @@ export const getSewRepEffforCt = async (req, res) => {
 };
 
 
+export const getListCtHeader= async (req, res) => {
+  try {
+      const { schDate, sitename } = req.params;
+      const listCtSite = await db.query(qryListCtSite, {
+      replacements: {
+        schDate: schDate,
+        sitename: sitename,
+        // shift: shift,
+      },
+      type: QueryTypes.SELECT,
+    });
+
+    return res.json({data : listCtSite})
+  } catch (error) {
+    console.log(error);
+    return res.status(404).json({
+      message: "error processing get data CT Site",
+      data: error,
+    });
+  }
+}
+
+export const deleteCtHeader= async (req, res) => {
+  try {
+      const { ctId } = req.params;
+     const deleteCtHeader = await IeCycleTimeHeader.destroy({
+      where : {
+        CT_ID : ctId
+      }
+     })
+
+     if(deleteCtHeader){
+      const checkMp = await IeCycleTimeMp.findAll({
+         where : {
+          CT_ID : ctId
+        },
+        raw : true
+      })
+
+      if(checkMp.length > 0){
+          const deleteMp = await IeCycleTimeMp.destroy({
+          where : {
+            CT_ID : ctId
+          }
+        })
+      }
+      const checkMpProccesses = await IeCtMpProcesses.findAll({
+         where : {
+          CT_ID : ctId
+        },
+          raw : true
+      })
+
+      if(checkMpProccesses.length > 0){
+          const deleteMpp = await IeCtMpProcesses.destroy({
+          where : {
+            CT_ID : ctId
+          }
+        })
+      }
+
+
+      const groupCount = await IeCtGroupCount.findAll({
+         where : {
+          CT_ID : ctId
+        },
+          raw : true
+      })
+
+      if(groupCount.length > 0){
+          const deletegroupCount = await IeCtGroupCount.destroy({
+          where : {
+            CT_ID : ctId
+          }
+        })
+      }
+
+      const checkDetailCount = await ieCtDetailCount.findAll({
+         where : {
+          CT_ID : ctId
+        },
+          raw : true
+      })
+
+      if(checkDetailCount.length > 0){
+          const deletegroupCount = await ieCtDetailCount.destroy({
+          where : {
+            CT_ID : ctId
+          }
+        })
+
+
+      }
+      
+      return res.json({message : 'Success Deleted CT'})
+
+     }else{
+        return res.status(404).json({message : 'Filed Delete'})
+
+      }
+
+  } catch (error) {
+    console.log(error);
+    return res.status(404).json({
+      message: "error processing get data CT Site",
+      data: error,
+    });
+  }
+}
+
+
 export const postHeaderIeCt = async (req, res, next) => {
   try {
     const dataPost = req.body 
 
+    const checkExist = await IeCycleTimeHeader.findOne({
+      where : { 
+        SCHD_ID : dataPost.SCHD_ID
+      } })
+
+    if(checkExist){
+      return res.status(202).json({message : 'Already Cycle Time '})
+    }
     const ieCtHeaderPost = await IeCycleTimeHeader.create(dataPost)
     if(ieCtHeaderPost){
       const dataReturn = ieCtHeaderPost.get({ plain: true });
@@ -241,7 +361,13 @@ export const postIeCtMp = async (req, res) => {
 
     const ieCtHeaderPost = await IeCycleTimeMp.bulkCreate(dataPostMp, { returning: true })
     if(ieCtHeaderPost){
-      
+          const ieCtHeaderPost = await IeCycleTimeMp.findAll({
+            where : {
+              CT_ID : dataPostMp[0].CT_ID
+            },
+            raw: true
+          })
+
       return res.json({message: 'success post Ie Cycletime manpower', data : ieCtHeaderPost})
     }else{
       return res.status(404).json({message : 'Failed to create new manpower CT'})
@@ -255,6 +381,122 @@ export const postIeCtMp = async (req, res) => {
     });
   }
 }
+
+export const deleteCtMp= async (req, res) => {
+  try {
+      const { ctMpId } = req.params;
+      
+      const findMp = await IeCycleTimeMp.findOne({
+            where : {
+              CT_MP_ID : ctMpId
+            },
+            raw: true
+        })
+
+
+      const deleteMp = await IeCycleTimeMp.destroy({
+            where : {
+              CT_MP_ID : ctMpId
+            }
+        })
+
+     if(deleteMp){
+
+      const checkMpProccesses = await IeCtMpProcesses.findAll({
+         where : {
+          CT_MP_ID : ctMpId
+        },
+          raw : true
+      })
+
+      if(checkMpProccesses.length > 0){
+          const deleteMpp = await IeCtMpProcesses.destroy({
+          where : {
+            CT_MP_ID : ctMpId
+          }
+        })
+      }
+
+
+      const groupCount = await IeCtGroupCount.findAll({
+         where : {
+          CT_MP_ID : ctMpId
+        },
+          raw : true
+      })
+
+      if(groupCount.length > 0){
+          const deletegroupCount = await IeCtGroupCount.destroy({
+          where : {
+            CT_MP_ID : ctMpId
+          }
+        })
+      }
+
+      const checkDetailCount = await ieCtDetailCount.findAll({
+         where : {
+          CT_MP_ID : ctMpId
+        },
+          raw : true
+      })
+
+      if(checkDetailCount.length > 0){
+          const deletegroupCount = await ieCtDetailCount.destroy({
+          where : {
+            CT_MP_ID : ctMpId
+          }
+        })
+
+      }
+
+         const getAllIeDetailCount = await IeCtMpProcessGroup.findAll({
+        where : {
+          CT_MP_ID : ctMpId
+        },
+        raw : true
+      })
+
+
+      if(getAllIeDetailCount.length > 0){
+          const delMppGrp = await IeCtMpProcessGroup.destroy({
+          where : {
+            CT_MP_ID : ctMpId
+          }
+        })
+
+      }
+
+       const ctMpList = await IeCycleTimeMp.findAll({
+            where : {
+              CT_ID : findMp.CT_ID
+            }
+          })
+        
+       const getDataAllMpp = await db.query(qryGetIctMpProcces, {
+          replacements: {ctId : findMp.CT_ID},
+          type: QueryTypes.SELECT,
+        })
+
+        const dataMpProc = getDataAllMpp || []
+
+      
+      return res.status(200).json({message : 'Success Deleted Manpower CT', ctMpList: ctMpList, dataMpProc : dataMpProc })
+
+     }else{
+      
+        return res.status(404).json({message : 'Filed Delete Manpower CT'})
+
+      }
+
+  } catch (error) {
+    console.log(error);
+    return res.status(404).json({
+      message: "error processing get Manpower CT",
+      data: error,
+    });
+  }
+}
+
 
 
 export const getBaseDataIeCyc = async (req, res) => {
@@ -308,8 +550,15 @@ export const getBaseDataIeCyc = async (req, res) => {
       type: QueryTypes.SELECT,
     })
 
+    const ctMppGrpAct = await IeCtMpProcessGroup.findAll({
+      where : {
+        CT_ID : dataCtHeader.CT_ID
+      }
+    })
+
     const detailSch = getDetailSch[0] || {}
     const dataMpProc = getDataAllMpp || []
+    const dataMppGrp = ctMppGrpAct || []
 
     const dataOb = {
         obHeader : obHeader[0] || {},
@@ -318,7 +567,7 @@ export const getBaseDataIeCyc = async (req, res) => {
         
       }
 
-    return res.json({dataCtHeader, detailSch, dataOb, dataCtMp, dataMpProc})
+    return res.json({dataCtHeader, detailSch, dataOb, dataCtMp, dataMpProc, dataMppGrp})
 
   } catch (error) {
     console.log(error);
@@ -464,7 +713,7 @@ export const getIeCtGroupCount = async(req,res) => {
   }
 }
 
-export const postIeCtDetailCount = async (req, res) => {
+export const postIeCtDetailCount = async (req, res, next) => {
   try {
     const dataBody = req.body
 
@@ -477,10 +726,27 @@ export const postIeCtDetailCount = async (req, res) => {
         raw : true
       })
 
+      if(getAllIeDtlCount.length === 5){
+          return res.status(202).json({message: "Max 5 time Detail"})
+      }
+
+      //check apakah kpo ambil time yang sama (untuk menghindari cheat)
+      const checkExistTime = getAllIeDtlCount.some(item => {
+        const dbTime = new Date(item.CT_DETAIL_GET_TIME).getTime();
+        const inputTime = new Date(dataBody.CT_DETAIL_GET_TIME).getTime();
+        return dbTime === inputTime;
+      });
+
+      if(checkExistTime){
+        return res.status(202).json({message: "Already get this time"})
+      }
+
+
       const dataPost = {
         ...dataBody,
         CT_DETAIL_NO : getAllIeDtlCount.length+1
       }
+      
 
     const postIeGc = await ieCtDetailCount.create(dataPost)
 
@@ -493,8 +759,10 @@ export const postIeCtDetailCount = async (req, res) => {
         },
         raw : true
       })
-
-      return  res.json({message : 'Success Add Detail Count', data : getAllIeDetailCount})
+      req.body.arrResult = getAllIeDetailCount
+      req.body.message = 'Success add detail'
+      next()
+      // return  res.json({message : 'Success Add Detail Count', data : getAllIeDetailCount})
     }else{
       return res.status(404).json({message : 'Filed Add Detail Count'})
     }
@@ -506,6 +774,180 @@ export const postIeCtDetailCount = async (req, res) => {
       }); 
   }
 }
+
+
+
+export const deleteIeCtDetailCount = async (req, res, next) => {
+  try {
+    const {ctDetailId} = req.params
+
+       const checkexistDtl = await ieCtDetailCount.findOne({
+        where : {
+          CT_DETAIL_ID : ctDetailId,
+        },
+        raw : true
+      })
+
+      if(!checkexistDtl){
+          return res.status(404).json({message: "No Data Detail"})
+      }
+
+      const deleteDetail = await ieCtDetailCount.destroy({
+            where : {
+              CT_DETAIL_ID : ctDetailId,
+            }
+          })
+
+
+    if(deleteDetail){
+      const getAllIeDetailCount = await ieCtDetailCount.findAll({
+        where : {
+          CT_ID : checkexistDtl.CT_ID,
+          CT_MP_ID : checkexistDtl.CT_MP_ID,
+          CT_MPP_ID : checkexistDtl.CT_MPP_ID,
+        },
+        raw : true
+      })
+
+      const dataBody = {
+        ...checkexistDtl,
+        arrResult : getAllIeDetailCount,
+        message: 'success delete detail'
+      }
+      req.body = dataBody
+      
+      next()
+      // return  res.json({message : 'Success Add Detail Count', data : getAllIeDetailCount})
+    }else{
+      return res.status(404).json({message : 'Filed delete Detail Count'})
+    }
+  } catch (error) {
+     console.log(error);
+      return res.status(404).json({
+        message: "error set delete Detail",
+        data: error,
+      }); 
+  }
+}
+
+
+
+export const midGetAvgMpp = async (req, res) => {
+  try {
+    const dataPost = req.body;
+    const arrResult = req.body.arrResult;
+
+     // Cek apakah sudah ada group yang cocok
+    const findMppGroup = await IeCtMpProcessGroup.findOne({
+      where: {
+        CT_ID: dataPost.CT_ID,
+        CT_MP_ID: dataPost.CT_MP_ID,
+        CT_MPP_ID: dataPost.CT_MPP_ID,
+        CT_GC_ID: dataPost.CT_GC_ID,
+      },
+      raw: true
+    });
+
+    //handle array kosong
+    if (!Array.isArray(arrResult) || arrResult.length === 0) {
+      // return res.status(400).json({ message: "arrResult is required and must be a non-empty array." });
+
+      //jika ada groupnya 
+      if(findMppGroup){
+          const patchMppGrp = await IeCtMpProcessGroup.destroy(
+            {
+              where: { CT_MPP_GC_ID: findMppGroup.CT_MPP_GC_ID }
+            }
+            );
+          }
+
+            const getAllMppGrp= await IeCtMpProcessGroup.findAll({
+            where : {
+              CT_ID : dataPost.CT_ID,
+            },
+            raw : true
+          })
+
+          return res.json({
+            message: req.body.message,
+            data: arrResult,
+            ctMppGroup : getAllMppGrp || []
+          });
+
+    }
+    
+    const avgMppTT = averageCol(arrResult, 'CT_DETAIL_TTIME');
+
+    const getDataOne = await db.query(qryGetIeCtMppOne, {
+      replacements: {
+        ctId: dataPost.CT_ID,
+        ctMpId: dataPost.CT_MPP_ID
+      },
+      type: QueryTypes.SELECT,
+    });
+
+    if (getDataOne.length === 0) {
+      return res.status(404).json({ message: "CT MPP data not found." });
+    }
+
+    const dataMpp = getDataOne[0];
+    // console.log({avgMppTT, count_using :dataMpp.COUNT_USING });
+    
+    const targetPcs =( (dataMpp.CT_WH * 60) / avgMppTT) / dataMpp.COUNT_USING;
+
+   
+
+    if (findMppGroup) {
+      const patchMppGrp = await IeCtMpProcessGroup.update(
+        {
+          CT_ACT_TAKE_TIME: avgMppTT,
+          CT_ACT_TARGET_PCS: targetPcs
+        },
+        {
+          where: { CT_MPP_GC_ID: findMppGroup.CT_MPP_GC_ID }
+        }
+      );
+
+      if (patchMppGrp[0] === 0) {
+        // Sequelize update returns [affectedCount]
+        throw new Error("Failed to update process group.");
+      }
+    } else {
+      const postMppGc = {
+        CT_ID: dataPost.CT_ID,
+        CT_MP_ID: dataPost.CT_MP_ID,
+        CT_MPP_ID: dataPost.CT_MPP_ID,
+        CT_GC_ID: dataPost.CT_GC_ID,
+        CT_ACT_TAKE_TIME: avgMppTT,
+        CT_ACT_TARGET_PCS: targetPcs,
+        ADD_ID: dataPost.ADD_ID
+      };
+
+      const createMppGrp = await IeCtMpProcessGroup.create(postMppGc);
+      if (!createMppGrp) {
+        throw new Error("Failed to create process group.");
+      }
+    }
+
+      const getAllMppGrp= await IeCtMpProcessGroup.findAll({
+        where : {
+          CT_ID : dataPost.CT_ID,
+        },
+        raw : true
+      })
+
+    return res.json({
+      message: req.body.message,
+      data: arrResult,
+      ctMppGroup : getAllMppGrp || []
+    });
+
+  } catch (error) {
+    console.error('Error in midGetAvgMpp:', error);
+    return res.status(500).json({ message: 'Internal server error', error: error.message });
+  }
+};
+
 
 export const getIeCtDetailCount = async(req,res) => {
   try {
@@ -528,6 +970,167 @@ export const getIeCtDetailCount = async(req,res) => {
       success: false,
       error: err,
       message: "errorget get detail ct",
+    });
+  }
+}
+
+
+export const getIeCtMppGroupCount = async(req,res) => {
+  try {
+    const {ctId} = req.params
+      const getAllIeDetailCount = await IeCtMpProcessGroup.findAll({
+        where : {
+          CT_ID : ctId,
+        },
+        raw : true
+      })
+    return res.status(200).json({
+      success: true,
+      message: "success get mpp group ct",
+      data: getAllIeDetailCount,
+    });
+  } catch(err){
+    res.status(404).json({
+      success: false,
+      error: err,
+      message: "errorget get mpp group ct",
+    });
+  }
+}
+
+
+function getCategoryStrings(data) {
+  const categoriesMap = {};
+
+  data.forEach(item => {
+    const mpId = item.CT_MP_ID;
+    const obDetailNo = item.OB_DETAIL_NO;
+
+    if (!categoriesMap[mpId]) {
+      categoriesMap[mpId] = new Set();
+    }
+
+    categoriesMap[mpId].add(obDetailNo);
+  });
+
+  const categoryStrings = Object.values(categoriesMap).map(obDetailSet => {
+    const sortedNos = Array.from(obDetailSet).sort((a, b) => a - b);
+    return sortedNos.join('+');
+  });
+
+  return categoryStrings;
+}
+
+
+
+export const getIeCtBarChartSeries = async(req,res) => {
+  try {
+    const {ctId} = req.params
+
+    const getHeaderCt = await IeCycleTimeHeader.findOne({
+      where : {
+        CT_ID : ctId
+      },
+      raw: true
+    })
+
+    const getDataOne = await db.query(qryGetDataBarChart, {
+      replacements: {
+        ctId: ctId,
+      },
+      type: QueryTypes.SELECT,
+    });
+
+
+    //untuk categories
+     const getDataAllMpp = await db.query(qryGetIctMpProcces, {
+      replacements: {ctId : ctId},
+      type: QueryTypes.SELECT,
+    })
+
+    const onlyGroupOne = getDataOne.filter(br => br.CT_GC_NO === 1)
+    //ambil array mpid untuk filter Mpp
+    const arrMpId = onlyGroupOne.map(br => br.CT_MP_ID)
+    const mppWithValue = getDataAllMpp.filter(mp => arrMpId.includes(mp.CT_MP_ID))
+
+   // time
+    const arrTargetTT = onlyGroupOne.map(br => br.CT_TAKET_TIME)
+    const arrStdTt = onlyGroupOne.map(br => getHeaderCt.CT_TAKE_TIME ? Math.round(getHeaderCt.CT_TAKE_TIME).toFixed() : 0)
+    const arrActTT = getDataOne.map(br => br.CT_ACT_TAKE_TIME)
+
+    // pcs
+    const arrStdPcs = onlyGroupOne.map(br => getHeaderCt.CT_TARGET ? getHeaderCt.CT_TARGET : 0)
+    const arrTargetPcs = onlyGroupOne.map(br => br.CT_TARGET_PCS)
+    const arrActTTPcs = getDataOne.map(br => br.CT_ACT_TARGET_PCS)
+
+    let seriesTT = []
+    if(arrStdTt.length > 0){
+      const objSeriesStd = {
+              name: 'STD',
+              type: 'line',
+              data: arrStdTt
+            }
+        seriesTT.push(objSeriesStd)
+    }
+
+    if(arrTargetTT.length > 0){
+      const objSeriesTgt = {
+              name: 'Target',
+              type: 'column',
+              data: arrTargetTT
+            }
+        seriesTT.push(objSeriesTgt)
+    }
+
+    if(arrActTT.length > 0){
+      const objSeriesTT = {
+              name: 'Actual',
+              type: 'column',
+              data: arrActTT
+            }
+        seriesTT.push(objSeriesTT)
+    }
+
+    let seriesPcs = []
+    if(arrStdPcs.length > 0){
+      const objSeriesStdPCs = {
+              name: 'STD',
+              type: 'line',
+              data: arrStdPcs
+            }
+        seriesPcs.push(objSeriesStdPCs)
+    }
+
+    if(arrTargetPcs.length > 0){
+      const objSeriesTgtPcs = {
+              name: 'Target',
+              type: 'column',
+              data: arrTargetPcs
+            }
+        seriesPcs.push(objSeriesTgtPcs)
+    }
+
+    if(arrActTTPcs.length > 0){
+      const objSeriesTTPcs = {
+              name: 'Actual',
+              type: 'column',
+              data: arrActTTPcs
+            }
+        seriesPcs.push(objSeriesTTPcs)
+    }
+
+    const categories = getCategoryStrings(mppWithValue)
+
+    return res.status(200).json({
+      success: true,
+      message: "success get mpp group ct",
+      data: {seriesTT, seriesPcs, categories},
+    });
+  } catch(err){
+    res.status(404).json({
+      success: false,
+      error: err,
+      message: "errorget get mpp group ct",
     });
   }
 }
