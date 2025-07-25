bbdd235f8e820b985aa12fbfada1fcad03c80856
Aan Kristanto Nugroho <aankristanto1@gmail.com>
2025-07-25 16:50:10 +0700

    add api post vendor detail

--- controllers/system/VendorDetail.js
+++ controllers/system/VendorDetail.js
@@ -24,24 +24,54 @@ export const getAllVendorDetail = async(req,res) => {
 export const postVendorDetail = async(req,res) => {
     try {
         const { dataVendor } = req.body;
-        if(dataVendor.VENDOR_ID==='<< NEW >>'){
+        if(!dataVendor.VENDOR_ID){
             const getLastVDCID = await ModelVendorDetail.findOne({
                 order: [['VENDOR_ID', 'DESC']],
                 raw: true
             }); 
-            const newIncrement = parseInt(getLastVDCID.WHC_ID.slice(-7)) + 1;
+            const newIncrement = !getLastVDCID ? '0000001' : parseInt(getLastVDCID.VENDOR_ID.slice(-7)) + 1;
             const newVDCID = 'VDC' + newIncrement.toString().padStart(7, '0');
             await ModelVendorDetail.create({
                 VENDOR_ID: newVDCID,
                 VENDOR_CODE: dataVendor.VENDOR_CODE,
                 VENDOR_NAME: dataVendor.VENDOR_NAME,
-                VENDOR_ACTIVE: dataVendor.VENDOR_ACTIVE
+                VENDOR_ACTIVE: dataVendor.VENDOR_ACTIVE || 1,
+                VENDOR_COMPANY_NAME: dataVendor.VENDOR_COMPANY_NAME,
+                VENDOR_PHONE: dataVendor.VENDOR_PHONE,
+                VENDOR_FAX: dataVendor.VENDOR_FAX,
+                VENDOR_WEB: dataVendor.VENDOR_WEB,
+                VENDOR_ADDRESS_1: dataVendor.VENDOR_ADDRESS_1,
+                VENDOR_ADDRESS_2: dataVendor.VENDOR_ADDRESS_2,
+                VENDOR_CITY: dataVendor.VENDOR_CITY,
+                VENDOR_PROVINCE: dataVendor.VENDOR_PROVINCE,
+                VENDOR_POSTAL_CODE: dataVendor.VENDOR_POSTAL_CODE,
+                VENDOR_COUNTRY_ID: dataVendor.VENDOR_COUNTRY_ID,
+                VENDOR_CONTACT_NAME: dataVendor.VENDOR_CONTACT_NAME,
+                VENDOR_CONTACT_PHONE_1: dataVendor.VENDOR_CONTACT_PHONE_1,
+                VENDOR_CONTACT_PHONE_2: dataVendor.VENDOR_CONTACT_PHONE_2,
+                VENDOR_CONTACT_EMAIL: dataVendor.VENDOR_CONTACT_EMAIL,
+                VENDOR_CLASS: dataVendor.VENDOR_CLASS
             });
         } else {
             await ModelVendorDetail.update({
                 VENDOR_CODE: dataVendor.VENDOR_CODE,
                 VENDOR_NAME: dataVendor.VENDOR_NAME,
-                VENDOR_ACTIVE: dataVendor.VENDOR_ACTIVE
+                VENDOR_ACTIVE: dataVendor.VENDOR_ACTIVE || 1,
+                VENDOR_COMPANY_NAME: dataVendor.VENDOR_COMPANY_NAME,
+                VENDOR_PHONE: dataVendor.VENDOR_PHONE,
+                VENDOR_FAX: dataVendor.VENDOR_FAX,
+                VENDOR_WEB: dataVendor.VENDOR_WEB,
+                VENDOR_ADDRESS_1: dataVendor.VENDOR_ADDRESS_1,
+                VENDOR_ADDRESS_2: dataVendor.VENDOR_ADDRESS_2,
+                VENDOR_CITY: dataVendor.VENDOR_CITY,
+                VENDOR_PROVINCE: dataVendor.VENDOR_PROVINCE,
+                VENDOR_POSTAL_CODE: dataVendor.VENDOR_POSTAL_CODE,
+                VENDOR_COUNTRY_ID: dataVendor.VENDOR_COUNTRY_ID,
+                VENDOR_CONTACT_NAME: dataVendor.VENDOR_CONTACT_NAME,
+                VENDOR_CONTACT_PHONE_1: dataVendor.VENDOR_CONTACT_PHONE_1,
+                VENDOR_CONTACT_PHONE_2: dataVendor.VENDOR_CONTACT_PHONE_2,
+                VENDOR_CONTACT_EMAIL: dataVendor.VENDOR_CONTACT_EMAIL,
+                VENDOR_CLASS: dataVendor.VENDOR_CLASS
             }, {
                 where: {
                     VENDOR_ID: dataVendor.VENDOR_ID,
@@ -53,6 +83,7 @@ export const postVendorDetail = async(req,res) => {
             message: "success post vendor detail",
         });
     } catch(err){
+        console.error(err);
         return res.status(500).json({
             success: false,
             error: err,
--- routes/system/vendordetail.route.js
+++ routes/system/vendordetail.route.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { getAllVendorDetail } from "../../controllers/system/VendorDetail.js";
+import { getAllVendorDetail, postVendorDetail } from "../../controllers/system/VendorDetail.js";
 
 const router = express.Router();
 
 router.get("/all", getAllVendorDetail);
-
+router.post("/", postVendorDetail);
 
 export default router;
