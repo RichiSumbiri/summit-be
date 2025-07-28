4341011af17fce8978c1c64db3acdf1e9828aebf
richi_sumbiri <richi.dharmaputra@sumbiritex.com>
2025-07-28 13:15:13 +0700

    adjust service attribute route

--- routes/system/serviceAttributes.route.js
+++ routes/system/serviceAttributes.route.js
@@ -13,7 +13,7 @@ router.get("/", getServiceAttributes);
 router.post("/", createServiceAttributes);
 router.post("/edit", editServiceAttributes);
 router.post("/delete", deleteServiceAttributes);
-router.get("/service-attributes/param", getServiceAttributesParam)
+router.get("/param", getServiceAttributesParam)
 
 //get custom query service attribute for service attribute values dropdown
 router.get("/dropdown-list", getServiceAttributesDropdown);
