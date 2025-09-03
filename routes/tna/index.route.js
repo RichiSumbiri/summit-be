13fb2d764fc4fd9d00f11d1d523a8f52561e37cd
richi_sumbiri <richi.dharmaputra@sumbiritex.com>
2025-09-03 10:30:49 +0700

    TNA Event Listing Report init

--- routes/tna/index.route.js
+++ routes/tna/index.route.js
@@ -2,6 +2,7 @@ import eventMasterRoute from "./eventMaster/eventMaster.route.js";
 import eventTemplateRoute from "./eventTemplate/eventTemplate.route.js";
 import eventFrameworkRoute from "./eventFramework/eventFramework.route.js";
 import eventManagementRoute from "./eventManagement/eventManagement.route.js";
+import reportsRoute from "./reports/reports.route.js";
 import express from "express";
 
 const router = express.Router();
@@ -17,5 +18,8 @@ router.use("/event-framework", eventFrameworkRoute);
 
 //EVENT MANAGEMENT
 router.use("/event-management", eventManagementRoute);
+
+//REPORTS
+router.use("/reports", reportsRoute);
 
 export default router;
